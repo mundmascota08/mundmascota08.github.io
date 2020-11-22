6d6186eb506fb15bbde8b679a10c8c56455b3714
mundmascota08 <74855434+mundmascota08@users.noreply.github.com>
2020-11-22 04:42:17 -0600

    Create juego.js

---
+++ js/juego.js
@@ -0,0 +1,111 @@
+// VARIABLES
+
+// Array de palabras
+var palabras = [["pitbull", "Raza de perro"], ["siames", "Raza de gato"], ["huella", "Parte de la pata"], ["maullido", "Comunicacion del gato"], ["ladrido", "Comunicacion del perro"], ["raton", "Comida del gato"], ["hueso", "Comida del perro"], ["neurotico", "Sociabilidad del gato"], ["amigable", "Sociabilidad del perro"], ["felino", "Familia del gato"], ["canino", "Familia del perro"], ["minino", "Sobrenombre del gato"], ["guagua", "Sobrenombre del perro"]];
+// Palabra a averiguar
+var palabra = "";
+// Nº aleatorio
+var rand;
+// Palabra oculta
+var oculta = [];
+// Elemento html de la palabra
+var hueco = document.getElementById("palabra");
+// Contador de intentos
+var cont = 6;
+// Botones de letras
+var buttons = document.getElementsByClassName('letra');
+// Boton de reset
+var btnInicio = document.getElementById("reset");
+
+// FUNCIONES
+
+// Escoger palabra al azar
+function generaPalabra() {
+  rand = (Math.random() * 19).toFixed(0);
+  palabra = palabras[rand][0].toUpperCase();
+  console.log(palabra);
+}
+
+// Funcion para pintar los guiones de la palabra
+function pintarGuiones(num) {
+  for (var i = 0; i < num; i++) {
+    oculta[i] = "_";
+  }
+  hueco.innerHTML = oculta.join("");
+}
+
+//Generar abecedario
+function generaABC (a,z) {
+  document.getElementById("abcdario").innerHTML = "";
+  var i = a.charCodeAt(0), j = z.charCodeAt(0);
+  var letra = "";
+  for( ; i<=j; i++) {
+    letra = String.fromCharCode(i).toUpperCase();
+    document.getElementById("abcdario").innerHTML += "<button value='" + letra + "' onclick='intento(\"" + letra + "\")' class='letra' id='"+letra+"'>" + letra + "</button>";
+    if(i==110) {
+      document.getElementById("abcdario").innerHTML += "<button value='Ñ' onclick='intento(\"Ñ\")' class='letra' id='"+letra+"'>Ñ</button>";
+    }
+  }
+}
+
+// Chequear intento
+function intento(letra) {
+  document.getElementById(letra).disabled = true;
+  if(palabra.indexOf(letra) != -1) {
+    for(var i=0; i<palabra.length; i++) {
+      if(palabra[i]==letra) oculta[i] = letra;
+    }
+    hueco.innerHTML = oculta.join("");
+    document.getElementById("acierto").innerHTML = "Bien!";
+    document.getElementById("acierto").className += "acierto verde";
+  }else{
+    cont--;
+    document.getElementById("intentos").innerHTML = cont;
+    document.getElementById("acierto").innerHTML = "Fallo!";
+    document.getElementById("acierto").className += "acierto rojo";
+    document.getElementById("image"+cont).className += "fade-in";
+  }
+  compruebaFin();
+  setTimeout(function () { 
+    document.getElementById("acierto").className = ""; 
+  }, 800);
+}
+
+// Obtener pista
+function pista() {
+  document.getElementById("hueco-pista").innerHTML = palabras[rand][1];
+}
+
+// Compruba si ha finalizado
+function compruebaFin() {
+  if( oculta.indexOf("_") == -1 ) {
+    document.getElementById("msg-final").innerHTML = "Bien hecho !!";
+    document.getElementById("msg-final").className += "zoom-in";
+    document.getElementById("palabra").className += " encuadre";
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    document.getElementById("reset").innerHTML = "Empezar";
+    btnInicio.onclick = function() { location.reload() };
+  }else if( cont == 0 ) {
+    document.getElementById("msg-final").innerHTML = "Fin del juego";
+    document.getElementById("msg-final").className += "zoom-in";
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    document.getElementById("reset").innerHTML = "Empezar";
+    btnInicio.onclick = function () { location.reload() };
+  }
+}
+
+// Restablecer juego
+function inicio() {
+  generaPalabra();
+  pintarGuiones(palabra.length);
+  generaABC("a","z");
+  cont = 6;
+  document.getElementById("intentos").innerHTML=cont;
+}
+
+// Iniciar
+window.onload = inicio();
